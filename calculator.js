56385167172490b37d2495fe2c7a0d00c705a7c1
Culbert Willoughby <culbertw@gmail.com>
2024-04-03 21:01:43 -0500

    feat: Add operate() function to select an operation to perform based on operator
    - Add variables operand1, operator, and operand2 to store user input.
    - operate() will perform a math operation based on the operator selected.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// parts of the calculator operation
+let operand1, operator, operand2;
+
 // functions for basic math operators
 function add(operand1, operand2) {
     return operand1 + operand2;
@@ -19,4 +22,21 @@ function divide(operand1, operand2) {
     }
 
     return operand1 / operand2;
+}
+
+// will call one of the operation functions based on the selected operator
+function operate(operand1, operator, operand2) {
+    switch (operator) {
+        case "+":
+            add(operand1, operand2);
+            break;
+        case "-":
+            subtract(operand1, operand2);
+            break;
+        case "x":
+            multiply(operand1, operand2);
+            break;
+        case "/":
+            divide(operand1, operand2);
+    }
 }
